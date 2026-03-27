304bc56951cef2ec2cabcdeeaa3f0868b8343bca
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-03-27 15:13:40 +0530

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,41 +1,38 @@
 import java.util.Scanner;
 
-public class GenericsMaxElement {
-
+public class MaxElementGeneric {
+    
+    // Generic method to find max
     public static <T extends Comparable<T>> T findMax(T[] array) {
-        if (array == null || array.length == 0) {
-            return null;
-        }
-        
         T max = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i].compareTo(max) > 0) {
-                max = array[i];
+        for (T element : array) {
+            if (element.compareTo(max) > 0) {
+                max = element;
             }
         }
         return max;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        if (scanner.hasNextInt()) {
-            int n = scanner.nextInt();
-            Integer[] intArray = new Integer[n];
-            for (int i = 0; i < n; i++) {
-                intArray[i] = scanner.nextInt();
-            }
-
-            int m = scanner.nextInt();
-            String[] strArray = new String[m];
-            for (int i = 0; i < m; i++) {
-                strArray[i] = scanner.next();
-            }
-
-            System.out.println("Max Integer: " + findMax(intArray));
-            System.out.println("Max String: " + findMax(strArray));
+        // Integer array
+        int n = sc.nextInt();
+        Integer[] intArr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArr[i] = sc.nextInt();
         }
 
-        scanner.close();
+        // String array
+        int m = sc.nextInt();
+        String[] strArr = new String[m];
+        for (int i = 0; i < m; i++) {
+            strArr[i] = sc.next();
+        }
+
+        sc.close();
+
+        System.out.println("Max Integer: " + findMax(intArr));
+        System.out.println("Max String: " + findMax(strArr));
     }
 }
